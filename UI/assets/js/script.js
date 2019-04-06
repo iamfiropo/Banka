34673a78081c6dc89c7a5666e19f3dd39ab3b4eb
Johnpeace <princeolatjohnjoe@yahoo.com>
2019-04-06 18:39:47 +0100

    feature(user status): add user status create user status button to activate and deactivate a user account [Finishes #164847938]

--- UI/assets/js/script.js
+++ UI/assets/js/script.js
@@ -1,7 +1,17 @@
-
+// transaction type modal
 const modal = document.querySelector(".mymodal");
-
 function openModal(bool){
     bool ? modal.style.display = "block"
     : modal.style.display = 'none';
+}
+
+//user status toggle
+activatebtn = document.querySelector('.activatebtn');
+function status(){
+    if (activatebtn.value === 'Activate'){
+        (activatebtn.style.color = 'red') && (activatebtn.value = 'Deactivate')
+    }
+    else {
+        (activatebtn.style.color = 'black') && (activatebtn.value = 'Activate')
+    }
 }
